9678e0dd24269822193ec3b41d18c0a974b7dccc
Bibek Panthi <bpanthi977@gmail.com>
2026-05-21 11:41:02 -0500

    Config changes

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rclone copy -P --exclude ".venv/" --exclude ".git/" --exclude "__pycache__/" ./ sp-collab:/post-sparse
+rclone copy -P --exclude ".venv/" --exclude ".git/" --exclude "__pycache__/" --exclude "wandb/" --exclude "logs/" ./ sp-collab:/post-sparse
